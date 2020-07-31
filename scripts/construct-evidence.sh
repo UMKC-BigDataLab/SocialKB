ef095bb14ca23fd0878f020275642144c9b173e6
Arun George Zachariah <azachariah@mail.missouri.edu>
2020-07-31 18:36:14 -0500

    Update construct-evidence.sh

--- scripts/construct-evidence.sh
+++ scripts/construct-evidence.sh
@@ -15,8 +15,7 @@ TWEETS_OUTPUT_DIR=$HOME/Tweets_Output
 EVIDENCE_DIR=$HOME/Evidence_Data
 
 # Build CollectData.
-cd ../CollectData
-#cd ../CollectData && sbt clean assembly
+cd ../CollectData && sbt clean assembly
 
 # Collect tweets.
 spark-submit --class edu.umkc.CollectTweets target/scala-2.11/CollectData-assembly-0.1.jar $TWEETS_PARTS_DIR $INTERVAL_IN_SECONDS $NUM_TO_COLLECT $CONSUMER_KEY $CONSUMER_SECRET $ACCESS_TOKEN $ACCESS_TOKEN_SECRET
@@ -25,8 +24,7 @@ spark-submit --class edu.umkc.CollectTweets target/scala-2.11/CollectData-assemb
 cd ../scripts/tweet-processing && ./tweets-copier.sh $SOFT_LIMIT $TWEETS_PARTS_DIR $TWEETS_OUTPUT_DIR
 
 # Build BuildEvidence
-cd ../../BuildEvidence
-#cd ../../BuildEvidence && sbt clean assembly
+cd ../../BuildEvidence && sbt clean assembly
 
 # Construct the evidence
 cd ../scripts/tweet-processing && bash gen-dbs.sh $TWEETS_OUTPUT_DIR $EVIDENCE_DIR $CONSUMER_KEY $CONSUMER_SECRET $ACCESS_TOKEN $ACCESS_TOKEN_SECRET
